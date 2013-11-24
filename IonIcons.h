d487a8359aadd06c8d5e68885b48fdbcdf83d223
Connor Grady <conair360@gmail.com>
2013-11-23 22:16:08 -0500

    Added NSAttributedString and UIBarButtonItem generators

--- IonIcons.h
+++ IonIcons.h
@@ -4,9 +4,19 @@
 
 #import "IonIcons-codes.h"
 
+typedef enum {
+    IonIconPositionLeft,
+    IonIconPositionRight
+} IonIconPosition;
+
 @interface IonIcons : NSObject
 
 + (UIFont *)fontWithSize:(CGFloat)fontSize;
+
+// NSAttributedString+IonIcons
++ (NSAttributedString *)attributedStringWithIcon:(NSString *)icon text:(NSString *)text size:(CGFloat)size;
++ (NSAttributedString *)attributedStringWithIcon:(NSString *)icon iconPosition:(IonIconPosition)iconPosition iconColor:(UIColor *)iconColor iconSize:(CGFloat)iconSize
+                                            text:(NSString *)text textAttributes:(NSDictionary *)textAttributes;
 
 // UIImage+IonIcons
 + (UIImage *)imageWithIcon:(NSString *)icon color:(UIColor *)color fontSize:(CGFloat)fontSize imageSize:(CGSize)imageSize;
@@ -16,6 +26,8 @@
 + (UILabel *)autoresizingLabelWithIcon:(NSString *)icon fontSize:(CGFloat)fontSize color:(UIColor *)color;
 
 // UIBarButtonItem+IonIcons
++ (UIBarButtonItem *)barButtonItemWithIcon:(NSString *)icon target:(id)target action:(SEL)action;  // color = white
 + (UIBarButtonItem *)barButtonItemWithIcon:(NSString *)icon color:(UIColor *)color target:(id)target action:(SEL)action;
++ (UIBarButtonItem *)barButtonItemWithIcon:(NSString *)icon title:(NSString *)title target:(id)target action:(SEL)action;
 
 @end
--- IonIcons.m
+++ IonIcons.m
@@ -4,7 +4,7 @@
 @implementation IonIcons
 
 + (UIFont *)fontWithSize:(CGFloat)fontSize {
-    
+    /*
     // List all fonts on iPhone
     NSArray *familyNames = [[NSArray alloc] initWithArray:[UIFont familyNames]];
     NSArray *fontNames;
@@ -17,8 +17,35 @@
         for (indFont=0; indFont<[fontNames count]; ++indFont)
             NSLog(@"    Font name: %@", [fontNames objectAtIndex:indFont]);
     }
-    
+    */
     return [UIFont fontWithName:@"Ionicons" size:fontSize];
+}
+
+
+
+#pragma mark - NSAttributedString
+
++ (NSAttributedString *)attributedStringWithIcon:(NSString *)icon text:(NSString *)text size:(CGFloat)size {
+    NSMutableAttributedString *attributedString = [[NSMutableAttributedString alloc] initWithString:[NSString stringWithFormat:@"%@ %@", icon, text]
+                                                                                         attributes:@{ NSFontAttributeName:[UIFont systemFontOfSize:size] }];
+    [attributedString addAttribute:NSFontAttributeName value:[self fontWithSize:size] range:NSMakeRange(0, 1)];
+    return attributedString;
+}
+
++ (NSAttributedString *)attributedStringWithIcon:(NSString *)icon iconPosition:(IonIconPosition)iconPosition iconColor:(UIColor *)iconColor iconSize:(CGFloat)iconSize
+                                            text:(NSString *)text textAttributes:(NSDictionary *)textAttributes
+{
+    NSString *string;
+    if (iconPosition == IonIconPositionLeft)        string = [NSString stringWithFormat:@"%@ %@", icon, text];
+    else if (iconPosition == IonIconPositionRight)  string = [NSString stringWithFormat:@"%@ %@", text, icon];
+    else return nil;
+    NSMutableAttributedString *attributedString = [[NSMutableAttributedString alloc] initWithString:string attributes:textAttributes];
+    
+    NSRange iconRange = NSMakeRange(0, 1);
+    if (iconPosition == IonIconPositionRight) iconRange.location = attributedString.length - 1;
+    [attributedString addAttributes:@{ NSForegroundColorAttributeName:iconColor, NSFontAttributeName:[self fontWithSize:iconSize] } range:iconRange];
+    
+    return attributedString;
 }
 
 
@@ -45,14 +72,32 @@
 
 #pragma mark - UIBarButtonItem+IonIcons
 
++ (UIBarButtonItem *)barButtonItemWithIcon:(NSString *)icon target:(id)target action:(SEL)action {
+    return [self barButtonItemWithIcon:icon color:[UIColor whiteColor] target:target action:action];
+}
+
 + (UIBarButtonItem *)barButtonItemWithIcon:(NSString *)icon color:(UIColor *)color target:(id)target action:(SEL)action {
     UIButton *btn = [UIButton new];
     [btn addTarget:target action:action forControlEvents:UIControlEventTouchUpInside];
+    btn.contentVerticalAlignment = UIControlContentVerticalAlignmentCenter;
     btn.showsTouchWhenHighlighted = YES;
-    btn.titleLabel.font = [self fontWithSize:32];
+    btn.titleLabel.font = [self fontWithSize:28];
     btn.titleLabel.textAlignment = NSTextAlignmentCenter;
     [btn setTitleColor:color forState:UIControlStateNormal];
     [btn setTitle:icon forState:UIControlStateNormal];
+    [btn sizeToFit];
+    return [[UIBarButtonItem alloc] initWithCustomView:btn];
+}
+
++ (UIBarButtonItem *)barButtonItemWithIcon:(NSString *)icon title:(NSString *)title target:(id)target action:(SEL)action {
+    UIButton *btn = [UIButton new];
+    [btn addTarget:target action:action forControlEvents:UIControlEventTouchUpInside];
+    btn.contentVerticalAlignment = UIControlContentVerticalAlignmentCenter;
+    btn.showsTouchWhenHighlighted = YES;
+    
+    NSAttributedString *string = [self attributedStringWithIcon:icon text:title size:18];
+    [btn setAttributedTitle:string forState:UIControlStateNormal];
+    
     [btn sizeToFit];
     return [[UIBarButtonItem alloc] initWithCustomView:btn];
 }
